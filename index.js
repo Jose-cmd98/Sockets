ae49778ad58629c682ee1d29e936150023cb8def
Jose-cmd98 <jose1234roberto@gmail.com>
2021-09-30 19:29:20 -0300

    connection event for incoming sockets and log it to console

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
+
 const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
 
-app.get('.', (req,res)=>{
+app.get('/', (req,res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
+io.on('connection', (socket) => {
+    console.log('Usuário Conectado. ');
+})
 
 http.listen(3000, () => {
     console.log('Listening on *:3000');
